55f2a07f2d9bf9d89a1eeffd5eab617987eb3de5
Tyler Poon <tylerbill99@hotmail.com>
2023-07-27 23:10:36 -0400

    add image scraping

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,10 +1,11 @@
 import { MangaRepository, Database, MangaSyncOptions } from "@ca-tyler/smithers-server-utils"
+import Bottleneck from "bottleneck"
 
 const script = async () => {
   let db: Database | null = null
   try {
     db = await Database.getInstance()
-    await MangaRepository.syncManga(db, new MangaSyncOptions({}))
+    await MangaRepository.syncManga(db, new MangaSyncOptions({onlyLatest: true}))
     await db.end()
   } catch (err) {
     console.log("Unexpected problem encountered when crawling")
